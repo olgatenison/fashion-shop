d0dcb38a4143f0c70d58b8b12da528d3eab1dd0d
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-02-23 22:56:51 +0200

    footer hovers

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,24 +11,41 @@ export default function Footer() {
         <div className="footer__social">
           <h3 className="footer__title">U.CLOTHES</h3>
           <div className="footer__desc">
-            Complete your style with awesome <br /> clothes from us.
+            Complete your style with <span>awesome</span> <br /> clothes from
+            us.
           </div>
           <div className="wrap_svg">
-            <img src={fb} alt="fb" width={52} height={52} />
-            <img src={linkedin} alt="linkedin" />
-            <img src={insta} alt="instagram" />
-            <img src={tw} alt="twitter" />
+            <a href="/" className="wrap_soc">
+              <img src={fb} alt="fb" width={52} height={52} />
+            </a>
+            <a href="/" className="wrap_soc">
+              <img src={linkedin} alt="linkedin" />
+            </a>
+            <a href="/" className="wrap_soc">
+              <img src={insta} alt="instagram" />
+            </a>
+            <a href="/" className="wrap_soc">
+              <img src={tw} alt="twitter" />
+            </a>
           </div>
         </div>
         <div className="footer_nav">
           <ul className="footer_nav-title">
             Company
-            <li>About</li>
-            <li>Contact us</li>
-            <li>Support</li>
-            <li>Careers</li>
+            <li className="footer_nav-item">
+              <a href="/">About</a>
+            </li>
+            <li className="footer_nav-item">
+              <a href="/">Contact us</a>
+            </li>
+            <li className="footer_nav-item">
+              <a href="/">Support</a>
+            </li>
+            <li className="footer_nav-item">
+              <a href="/">Careers</a>
+            </li>
           </ul>
-          <ul className="footer_nav-title">
+          {/* <ul className="footer_nav-title">
             Quick Link
             <li>Share Location</li>
             <li>Orders Tracking</li>
@@ -39,7 +56,7 @@ export default function Footer() {
             Legal
             <li>Terms & conditions</li>
             <li>Privacy Policy</li>
-          </ul>
+          </ul> */}
         </div>
       </section>
     </>
