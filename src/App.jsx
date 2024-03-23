7cd2a701164c129ad78ee917b79c65deff483d3b
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-03-23 23:51:26 +0200

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/header/Header';
 import Promo from 'components/promo/Promo';
-import Brands from 'components/brands/Brands';
+// import Brands from 'components/brands/Brands';
 import Arrivals from 'components/arrivals/Arrivals';
 import Sale from 'components/sale/Sale';
 // import Login from 'pages/login/Login';
@@ -15,7 +15,7 @@ export const App = () => {
 
       <Header />
       <Promo />
-      <Brands />
+      {/* <Brands /> */}
       <Arrivals />
       <Sale />
       <Favorite />
