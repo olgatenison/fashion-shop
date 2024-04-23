32944dc4b58c462443dd9bcb59082fae9b8a5ab9
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-04-23 23:47:54 +0300

    adding home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-import Header from './components/header/Header';
-import Promo from 'components/promo/Promo';
-import Brands from 'components/brands/Brands';
-import Arrivals from 'components/arrivals/Arrivals';
-import Sale from 'components/sale/Sale';
 // import Login from 'pages/login/Login';
-import Community from 'components/community/Community';
+import Header from './components/header/Header';
+import { Home } from 'pages/Home';
 import Footer from 'components/footer/Footer';
-import Favorite from 'components/favourite/Favorite';
 
 export const App = () => {
   return (
     <div className="App">
       {/* <Login /> */}
       <Header />
-      <Promo />
-      <Brands />
-      <Arrivals />
-      <Sale />
-      <Favorite />
-
-      <Community />
+      <Home />
       <Footer />
     </div>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Promo from 'components/promo/Promo';
+import Brands from 'components/brands/Brands';
+import Arrivals from 'components/arrivals/Arrivals';
+import Sale from 'components/sale/Sale';
+import Community from 'components/community/Community';
+import Favorite from 'components/favourite/Favorite';
+
+export const Home = () => {
+  return (
+    <>
+      <Promo />
+      <Brands />
+      <Arrivals />
+      <Sale />
+      <Favorite />
+      <Community />
+    </>
+  );
+};
