94f97f83f8f7d1cb990fdf47ac9493693571e062
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-04-25 21:56:10 +0300

    fixing header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 // import Login from 'pages/login/Login';
 import Header from './components/header/Header';
-// import { Home } from 'pages/Home';
+import { Home } from 'pages/Home';
 import Footer from 'components/footer/Footer';
 import { Catalouge } from 'pages/Catalogue';
 
 export const App = () => {
   return (
-    <div className="App">
+    <div className="App ">
       {/* <Login /> */}
       <Header />
+      <Home />
       <Catalouge />
-      {/* <Home /> */}
       <Footer />
     </div>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
   };
 
   return (
-    <header className="header">
+    <header className="header ">
       <div className="container">
         <div className="header__row">
           <div className="header__logo">
