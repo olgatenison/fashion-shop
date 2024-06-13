fa12e5c9a20d6fecb798fa14176c6d63f843fe8c
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-06-14 00:26:15 +0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 // import Login from 'pages/login/Login';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
-import { Home } from 'pages/Home';
 import Footer from 'components/footer/Footer';
-import { Catalouge } from 'pages/Catalogue';
+import { CatalogePage } from './pages/CatalogePage';
+import { HomePage } from 'pages/HomePage';
+import { FavouritePage } from 'pages/FavouritePage';
 
 export const App = () => {
   return (
-    <div className="App ">
-      {/* <Login /> */}
-      <Header />
-      <Home />
-      <Catalouge />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App ">
+        {/* <Login /> */}
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalogue" element={<CatalogePage />} />
+          <Route path="/favourites" element={<FavouritePage />} />
+          {/* <Route path="/login" element={<Login/>} /> */}
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 };
--- src/pages/FavouritePage.jsx
+++ src/pages/FavouritePage.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export const FavouritePage = () => {
-  return <>Favourite Items</>;
+  return (
+    <>
+      <div>FavouritePage</div>
+    </>
+  );
 };
