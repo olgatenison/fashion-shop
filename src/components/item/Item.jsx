0510e4f5dd40ff8ee5a84d0cb111980d7a2e7469
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-06-17 22:33:18 +0300

    item title

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -5,6 +5,11 @@ import arrowImg from './../../img/icons/arrow.svg';
 const Card = () => {
   return (
     <div className={styles.item__wrapper}>
+      <div></div>
+      <div>
+        <h3>Name of item</h3>
+        <p></p>
+      </div>
       <div className={styles.item__icon}>
         <img src={arrowImg} alt="Open" />
       </div>
